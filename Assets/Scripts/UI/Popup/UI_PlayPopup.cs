3cd0ce305ba78a46f27b24a7068ab4ca399b542f
선재영 <wodud1892@naver.com>
2024-11-28 02:19:48 +0900

    ✨ update : 아이템 획득 노선까지 업데이트

--- Assets/Scripts/UI/Popup/UI_PlayPopup.cs
+++ Assets/Scripts/UI/Popup/UI_PlayPopup.cs
@@ -13,26 +13,15 @@ public class UI_PlayPopup : UI_Popup
         if (base.Init() == false)
 			return false;
 
-        Player = GameObject.FindGameObjectWithTag("Player");
         
-        if(Player != null)
-        {
-            GameObject customManager = Utils.FindChild(Player, "CustomManager");
-            //스크립트 접근
-            CustomManager _customManager = customManager.GetComponent<CustomManager>();
-
-            _customManager.hair = 6;
-            Debug.Log("111");
-            _customManager.clothes = UI_NamePopup.playerIndex[1]; 
-            _customManager.eyebrow = UI_NamePopup.playerIndex[2]; 
-            _customManager.eye = UI_NamePopup.playerIndex[3]; 
-            _customManager.mouth = UI_NamePopup.playerIndex[4]; 
-            _customManager.emotion = UI_NamePopup.playerIndex[5]; 
-
-        }
+        
+        Player = Managers.Resource.Instantiate("StaticPlayer");
+        Player.transform.position = new Vector3(-4,-4,0);
+        Player.transform.localScale = new Vector3(1,1, 1);
+        
 
         Stranger = Managers.Resource.Instantiate("Stranger");
-        Stranger.transform.position = new Vector3(-10,0,0);
+        Stranger.transform.position = new Vector3(-10,2,0);
 
         return true;
     }
@@ -44,10 +33,9 @@ public class UI_PlayPopup : UI_Popup
             if(Stranger.transform.position.x >= 10)
             {
                 Debug.Log("ENDENDEND");
-                // Managers.UI.ClosePopupUI(this);
-                // Managers.UI.ShowPopupUI<UI_TitlePopup>();
+                Managers.UI.ClosePopupUI(this);
+                Managers.UI.ShowPopupUI<UI_GuessPopup>();
 
-                
                 Managers.Resource.Destroy(Stranger);
         }
         }
